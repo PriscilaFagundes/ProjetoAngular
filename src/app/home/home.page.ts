6980636a2b35e7982ee923ecb3159a414ef8799a
PriscilaFagundes <125563142+PriscilaFagundes@users.noreply.github.com>
2023-06-27 13:51:58 +0000

    calculadora2

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 
@@ -6,8 +7,34 @@ import { IonicModule } from '@ionic/angular';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, CommonModule],
 })
 export class HomePage {
-  constructor() {}
+
+  resultado: number = 0;
+   
+  soma(){}
+
+  multiplicar(){}
+
+  divisao(){}
+
+  excluir(){}
+
+
+
+
+
+
+	
+
+	exibir(numero: number) {
+		// usamos this para acessar a variável que foi criada acima
+		this.resultado = numero * numero;
+
+		Number(this.resultado)
+	}
+
 }
+
+
